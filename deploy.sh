c20c36804b9bca441d9e8c582ee004dfbd8bc929
Jakob Høgenes <1014990+jakhog@users.noreply.github.com>
2019-01-30 14:33:15 +0100

    Change deploy script to agree

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 docker push dolittle/signup
-kubectl patch deployment signup --namespace dolittle -p "{\"spec\":{\"template\":{\"metadata\":{\"labels\":{\"date\":\"`date +'%s'`\"}}}}}"
+kubectl patch deployment signup --namespace dolittle-campaigns -p "{\"spec\":{\"template\":{\"metadata\":{\"labels\":{\"date\":\"`date +'%s'`\"}}}}}"
